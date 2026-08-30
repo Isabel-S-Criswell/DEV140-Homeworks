d7776c345c7b00368596bb5faf542e2c904a2602
Isabel Criswell <iscriswell@gmail.com>
2026-08-30 11:56:31 -0400

    Updated Code.gs file and linked new deployed script

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    const SHEETS_API_URL = 'https://script.google.com/macros/s/AKfycbyHUioa23K3yTAyzFLiH-G_jojG73XhzgPgel6j2hoJn2feCYS7WhrDkscZoWJgiWDq/exec';
+    const SHEETS_API_URL = 'https://script.google.com/macros/s/AKfycbyHUioa23K3yTAyzFLiH-G_jojG73XhzgPgel6j2hoJn2feCYS7WhrDkscZoWJgiWDq/exechttps://script.google.com/macros/s/AKfycbyKsi6ihlH_2zhXgNdn2sNUPAVrfZhsKXm38u63Edr_18J9klwtlzXF8MNUZHBWamR2/exec';
 
     let rawAssignments = [];
     let selectedWeek = 'ALL';
